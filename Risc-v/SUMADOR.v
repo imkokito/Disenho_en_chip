4f0c1bc6b6f66e1362394929c8cf8593b7cf8002
jared kokito <jared7616@gmail.com>
2026-05-29 20:25:20 -0600

    Update SUMADOR.v
    Added comments for clarity on inputs and outputs.

--- Risc-v/SUMADOR.v
+++ Risc-v/SUMADOR.v
@@ -1,8 +1,9 @@
+// codigo para poder sumarle cosas al program counter
 module SUMADOR(
-    input [31:0] A, B,
-    output [31:0] result
+    input [31:0] A, B, // 2 inputs de 32 bits 
+    output [31:0] result // 1 output también de 32 bits
 );
 
-assign result = A + B;
+assign result = A + B; // asignar el resultado de la suma a la variable de salida
 
 endmodule
