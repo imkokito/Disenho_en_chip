94113eceff50f48e09cd2e439df9604126adab43
jared kokito <jared7616@gmail.com>
2026-05-29 20:24:57 -0600

    Update REG_FILE.v
    Updated REG_FILE module with comments and formatting improvements.

--- Risc-v/REG_FILE.v
+++ Risc-v/REG_FILE.v
@@ -1,19 +1,20 @@
+//código para los registros, lee los registros y en caso de necesitarlo escribe en ellos
 module REG_FILE(
-    input clk, write_enable,
-    input [4:0] rs1, rs2, rd,
-    input [31:0] write_data,
-    output reg [31:0] RD1, RD2
+    input clk, write_enable, //clk y señal para habiliar que escriba
+    input [4:0] rs1, rs2, rd, //registros fuente y destino
+    input [31:0] write_data, //dato a escribir
+    output reg [31:0] RD1, RD2 //datos leidos 
 );
 
-reg [31:0] register [31:0]; 
+reg [31:0] register [31:0]; //vector de registros
 
 
 always @(*)
 begin
     if (rs1 == 0)
-        RD1 = 0;
+        RD1 = 0; 
     else
-        RD1 = register[rs1];
+        RD1 = register[rs1]; //si el registro fuente no es 0 devuelve el valor
 
     if (rs2 == 0)
         RD2 = 0;
@@ -21,6 +22,7 @@ begin
         RD2 = register[rs2];
 end
 
+//si la señal de escribir se activa y el registro no es 0, se escribe el dato 
 always @(posedge clk)
 begin
     if (write_enable == 1 && rd != 0)
