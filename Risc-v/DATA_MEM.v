687dd9db4e32b0d564a51d378be105d341ed0465
jared kokito <jared7616@gmail.com>
2026-05-29 20:23:11 -0600

    Update DATA_MEM.v
    Updated comments and formatting in DATA_MEM module.

--- Risc-v/DATA_MEM.v
+++ Risc-v/DATA_MEM.v
@@ -1,21 +1,23 @@
+//codigo para la memoria de datos
+//sirve para leer o escribir los datos 
 module DATA_MEM(
     input clk,
-    input WE,
-    input [31:0] addr, WD,
-    output [31:0] RD
+    input WE, //señal para escribir
+    input [31:0] addr, WD, //el dato y la direccion de donde escribir o leer
+    output [31:0] RD //el dato
 );
 
 reg [31:0] memory [0:255]; //vector de 256 palabras de 32 bits cada una
 
 integer i;
 
-initial begin
+initial begin //inicializar en 0 para no tener datos basura
     for (i=0; i<256; i=i+1) begin
         memory[i]=0; 
     end
 end
 
-always @(posedge clk) begin
+always @(posedge clk) begin //se escribe el dato si WE=1 usando addr[31:2] porque la memoria se direcciona por palabras
     if (WE) begin
         memory[addr[31:2]] <= WD; 
     end
